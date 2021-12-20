f5a1d827acd669f582426c17868a4f65c34fa1e0
Kristofar Stavrev <kristofur.stavrev@gmail.com>
2021-12-20 22:14:55 +0200

    Creation of Main Branch
    Initial commit of the two main scripts

---
+++ Dashboard/charts.R
@@ -0,0 +1,19 @@
+# Load libraries ----
+library(lubridate)
+library(dplyr)
+library(googleVis)
+library(shiny)
+# Import data ----
+rm(list=ls())
+setwd(paste(dirname(rstudioapi::getSourceEditorContext()$path),"/db",sep=""))
+
+csv_data=read.csv("db_raw_data.csv", stringsAsFactors = F, na.strings = c("NA", "", " "))
+
+# Check the class of each vector from the data frame
+aux_table = data.frame(nm=names(csv_data), cl=sapply(csv_data, class), nas=colSums(is.na(csv_data)))
+
+csv_data$Date = ymd(csv_data$Date)
+
+# Aggregate data ----
+visit_month = dd %>% group_by(Year, Month) %>% summarise(n_orders=n(),sum_sales=sum(Sales)) #%>% arrange(desc(sum_sales))
+dda= as.data.frame(dda)
